2a0ca8ec2a1877ed43bb98501bc9f63625a7aef1
ValeriaCom <78487304+ValeriaCom@users.noreply.github.com>
2021-02-22 20:27:00 +0100

    Metodo add()

--- CrudAngMac/src/app/app.component.ts
+++ CrudAngMac/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Prodotto } from './prodotto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CrudAngMac';
+  // proprietà funzionali dell'interfaccia utente
+  prodotto: Prodotto = new Prodotto();
+  prodotti: Prodotto[] = []; // il vero DB
+  righe: Prodotto[] = []; // la lista filtrata tramite ricerca
+  criterioRicerca: string = "";
+  rigaCorrente: number;
+  codice : Prodotto;
+  descrizione : Prodotto;
+
+  // stato della UI
+  stato: string = "RIC";
+
+  // proprietà per la gestione del layout della UI
+  showFormDati: boolean = false;
+  showModifica: boolean;
+  showConferma: boolean;
+  showAnnulla: boolean;
+  showRimuovi: boolean;
+  showNuova: boolean;
+  showSeleziona: boolean;
+  showRicerca: boolean = true;
+  showTable: boolean = false;
+  disableInputs: boolean = false;
+
+  add() {
+    switch (this.stato) {
+      case "RIC":
+        this.showFormDati = false;
+        this.showModifica = false;
+        this.showConferma = false;
+        this.showNuova = true;
+        this.showSeleziona = true;
+        this.showRicerca = true;
+        this.showTable = true;
+        this.stato = "AGG";
+        break;
+      default:
+        console.log("evento add imprevisto");
+        break;
+        case "VIS":
+          this.showFormDati = true;
+          this.showRicerca = false;
+          this.showNuova = false;
+          this.showConferma = true;
+          this.showAnnulla = true;
+          this.codice = true;
+          this.stato = "AGG";
+    }
+  }
 }
+
---
+++ CrudAngMac/src/app/prodotto.ts
@@ -0,0 +1,5 @@
+export class Prodotto{
+    codice: string="";
+    descrizione: string="";
+    id:number=0; //la gestisce il sistema: è l indice dentro l'array prodotti
+}
